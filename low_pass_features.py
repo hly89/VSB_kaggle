f973de53247b871a7f0912f7f595fc5056da6dc2
Liye He <heliye89@gmail.com>
2019-01-23 21:49:06 +0000

    extracting features from low-pass signals

--- low_pass_features.py
+++ low_pass_features.py
@@ -1,18 +1,21 @@
 from fft_dwt import *
 import pandas as pd
-import pyarrow.parquet as pq
+#import pyarrow.parquet as pq
 import numpy as np
 from numpy.fft import *
 from scipy import fftpack
 import pywt
 
-train_df = pq.read_pandas("../train.parquet").to_pandas()
-train_df = train_df.T
-meta_tr = pd.read_csv("../metadata_train.csv")
-# raw signal to low-pass signal with threshold 1e2
-lp = train_df.apply(lambda x: low_pass(x, 1e2), axis=1)
 
-lp_mean = lp.apply(np.mean, axis=1)
-lp_std = lp.apply(np.std, axis=1)
-lp_max = lp.apply(max, axis=1)
-lp_max_idx = lp.apply(np.argmax, axis=1)
+def lp_features_extraction(sig, thred):
+    # raw signal to low-pass signal with threshold 1e2/1e3
+    lp = sig.apply(lambda x: low_pass(x, thred), axis=1)
+    lp_mean = lp.apply(np.mean, axis=1)
+    lp_std = lp.apply(np.std, axis=1)
+    lp_max = lp.apply(max, axis=1)
+    lp_max_idx = lp.apply(np.argmax, axis=1)
+    lp_features = pd.concat([lp_max_idx, lp_max, lp_mean, lp_std], axis=1)
+    lp_features.columns=['lp_max_idx', 'lp_max', 'lp_mean', 'lp_std']
+
+    
+    return lp_features
